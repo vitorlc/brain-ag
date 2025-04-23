d05fa39a5566eda2d0873f16314148b53942dcdb
vitorlc <vitorcirqueira@gmail.com>
2025-04-23 17:57:39 -0300

    feat: define custom base entity

---
+++ src/utils/custom-base-entity.ts
@@ -0,0 +1,17 @@
+import {
+    BaseEntity,
+    PrimaryKey,
+    Property,
+  } from '@mikro-orm/core';
+  import { v4 as uuid } from 'uuid';
+  
+  export abstract class CustomBaseEntity extends BaseEntity {
+    @PrimaryKey()
+    id: string = uuid();
+  
+    @Property({ onCreate: () => new Date() })
+    createdAt: Date;
+  
+    @Property({ onCreate: () => new Date(), onUpdate: () => new Date() })
+    updatedAt: Date;
+  }
