41629b8ad60fd782064355296d0ba5c1b4152b4f
vitorlc <vitorcirqueira@gmail.com>
2025-04-24 17:47:29 -0300

    feat: add key in exists decorator

--- src/decorators/exists.decorator.ts
+++ src/decorators/exists.decorator.ts
@@ -1,15 +1,18 @@
-import { registerDecorator, ValidationOptions } from "class-validator";
-import { ExistsConstraint } from "src/validators/existsConstraint.validator";
+import { registerDecorator, ValidationOptions } from 'class-validator';
+import { ExistsConstraint } from 'src/validators/existsConstraint.validator';
 
-export function Exists(EntityClass: any, validationOptions?: ValidationOptions) {
-    return function (object: Object, propertyName: string) {
-      registerDecorator({
-        target: object.constructor,
-        propertyName,
-        options: validationOptions,
-        constraints: [EntityClass],
-        validator: ExistsConstraint,
-      });
-    };
-  }
-  
+export function Exists(
+  EntityClass: any,
+  key: string = 'id',
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [EntityClass, key],
+      validator: ExistsConstraint,
+    });
+  };
+}
--- src/validators/existsConstraint.validator.ts
+++ src/validators/existsConstraint.validator.ts
@@ -1,26 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import {
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
-  ValidationArguments,
-} from 'class-validator';
+import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
 import { EntityManager } from '@mikro-orm/postgresql';
 
 @ValidatorConstraint({ async: true })
-@Injectable()
 export class ExistsConstraint implements ValidatorConstraintInterface {
   constructor(private readonly em: EntityManager) {}
 
   async validate(value: any, args: ValidationArguments): Promise<boolean> {
-    const [EntityClass] = args.constraints;
+    const [EntityClass, key = 'id'] = args.constraints;
 
     if (!value) return false;
 
-    const found = await this.em.count(EntityClass, { id: value });
+    const found = await this.em.count(EntityClass, { [key]: value });
     return found > 0;
   }
 
   defaultMessage(args: ValidationArguments) {
-    return `${args.property} does not refer to an existing entity`;
+    const [, key = 'id'] = args.constraints;
+    return `${args.property} does not refer to an existing ${key}`;
   }
 }
