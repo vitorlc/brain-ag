f626c550366308c23969bb6962c60a2088126fe3
vitorlc <vitorcirqueira@gmail.com>
2025-04-25 09:26:29 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('Brain Agriculture')
+    .setDescription(
+      'API para gerenciamento de produtores, propriedades e culturas.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
