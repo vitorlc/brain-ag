147b29211af5e957cf40e1245f1b0aeb7012b9a0
vitorlc <vitorcirqueira@gmail.com>
2025-04-25 14:20:33 -0300

    feat: change db config to use envs

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,23 +1,26 @@
 import { MikroOrmModuleOptions } from '@mikro-orm/nestjs';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { config as loadEnv } from 'dotenv';
 import { Farmer } from './farmers/entities/farmer.entity';
 import { Farm } from './farms/entities/farm.entity';
 import { Harvest } from './cultivation/harvests/entities/harvest.entity';
 import { Crop } from './cultivation/crops/entities/crop.entity';
 
+loadEnv(); 
+
 const config: MikroOrmModuleOptions = {
   entities: [Farmer, Farm, Harvest, Crop],
-  dbName: 'brain_ag',
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
+  dbName: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: 5432,
   driver: PostgreSqlDriver,
-  debug: true,
+  debug: process.env.NODE_ENV !== 'production',
   forceEntityConstructor: true,
   strict: false,
   migrations: {
-    path: 'src/migrations', 
+    path: 'src/migrations',
   },
 };
 
